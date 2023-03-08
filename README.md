36e4a4b0aca5db30b73820bf99981ca9c7d8ee99
DavidDickens <gamrdave1@gmail.com>
2023-03-08 00:32:49 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Lab: "About me"
+
+# LAB - 2
+
+## About Me
+
+I build an app with the purpose of creating a page thats about me that quizzes the user. Doing so will force them to learn a little about me. 
+
+### Author: David Dickens
+
+### Links and Resources
+
+https://www.w3schools.com/css/default.asp 
+
+
+### Lighthouse Accessibility Report Score
+
+* Provide a screenshot of your score after running a Lighthouse Accessibility report.
+
+### Reflections and Comments
+
+* Consider including the answers to your daily journal and submission questions here
+* This is also a good place to reflect on the tools and resources used and learned
---
+++ app.js
@@ -0,0 +1,65 @@
+/* 'use strict'; // forces use to use proper keywords
+
+// ask the user their name with prompt()
+let name = prompt('What is your name?');
+console.log(name);
+//Display that name back to the user through a custom greeting welcoming them to your site.
+alert('Hey ' + name + 'thanks for stopping by!');
+
+
+// 5 questions y/n 
+
+let qOne = prompt('Do you think I am from the South?').toLowerCase();
+
+if (qOne === 'yes'){ 
+  console.log('I am');
+  alert('Very nice! I am from Florida.');
+} else {
+  alert('Sorry but thats wrong I was born in Florida.');
+}
+
+let qTwo = prompt('Do I have any siblings?').toLowerCase();
+if (qTwo === 'yes'){
+  console.log('Yep 4');
+  alert('True sadly I have 4 brothers.');
+} else {
+  alert('I wish that were true.');
+}
+
+let qThree = prompt('Did I ever work in the restaurant business?').toLowerCase();
+if (qThree === 'yes'){
+  console.log('true');
+  alert('I did, for far to long.');
+} else {
+  alert('Not quite I did for years.');
+}
+
+let qFour = prompt('Do I have and kids?').toLowerCase();
+if (qFour === 'no'){
+  console.log('Nice you got it!');
+  alert('No little ones yet, nice.');
+} else {
+  alert('Ha no kids I still have free time.');
+}
+
+let qFive = prompt('Was I in the military?').toLowerCase();
+if (qFive === 'yes'){
+  console.log('Its like you know me...');
+  alert('I was for a total of about 8 years.');
+} else {
+  alert('Sorry but I joined the Army at 19.');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
