7ba69efbe2e7fe39d9a6ea4d8b8d0aad91c1e063
DavidDickens <gamrdave1@gmail.com>
2023-03-10 00:51:51 +0000

    partner project

--- app.js
+++ app.js
@@ -1,42 +1,19 @@
-'use strict'; // forces use to use proper keywords
+'use strict';
 
-// ask the user their name with prompt()
+
 let name = prompt('What is your name?');
 console.log(name);
-//Display that name back to the user through a custom greeting welcoming them to your site.
+
 alert('Hey ' + name + ' thanks for stopping by!');
 
 
 
-let question = ['Do you think I am from the South?', 'Did I ever work in the restaurant business?','Do I have any siblings?', 'Do I have and kids?', 'Was I in the military?'];
+let question = ['Do you think I am from the South? yes/y or no/n', 'Did I ever work in the restaurant business? yes/y or no/n', 'Do I have any siblings? yes/y or no/n', 'Do I have and kids? yes/y or no/n', 'Was I in the military? yes/y or no/n'];
 
-/* let answer = ['yes', 'yes', 'yes', 'yes','yes','yes']; */
-
-function askQuestion(question, answer) {
-  let response = prompt(question).toLowerCase();
-  if (response === answer) {
-/*     return true;
-  } else {
-    return false;
-  } */
-}
-
-
-/* for (let i = 0; i < question.length; i++) {
-  console.log(i);
-  let rightAnswer = askQuestion(question[i], answer[i]);
-  if (rightAnswer === true) {
-    alert('good!');
-  } else {
-    alert('wrong');
-  }
-} */
-
-function test(){
+function aboutMe() {
   for (let i = 0; i < question.length; i++) {
     console.log(i);
-  let response = prompt(question[i]).toLowerCase();
-    //let rightAnswer = askQuestion(question[i], answer[i]);
+    let response = prompt(question[i]);
     if (response === 'yes' || response === 'y') {
       alert('good!');
     } else {
@@ -45,126 +22,67 @@ function test(){
   }
 }
 
-test();
+aboutMe();
 
 
 
-//askQuestion();
+function numberGame() {
+  let num = 5; // guessed number .
 
-/* response === answer[i]
-alert good
-alert wrong
- */
-/*
-
-// 5 questions y/n 
-
-let qOne = prompt('Do you think I am from the South?').toLowerCase();
-
-if (qOne === 'yes'){ 
-  console.log('I am');
-  alert('Very nice! I am from Florida.');
-} else {
-  alert('Sorry but thats wrong I was born in Florida.');
-}
-
-let qTwo = prompt('Do I have any siblings?').toLowerCase();
-if (qTwo === 'yes'){
-  console.log('Yep 4');
-  alert('True sadly I have 4 brothers.');
-} else {
-  alert('I wish that were true.');
-}
-
-let qThree = prompt('Did I ever work in the restaurant business?').toLowerCase();
-if (qThree === 'yes'){
-  console.log('true');
-  alert('I did, for far to long.');
-} else {
-  alert('Not quite I did for years.');
-}
-
-let qFour = prompt('Do I have and kids?').toLowerCase();
-if (qFour === 'no'){
-  console.log('Nice you got it!');
-  alert('No little ones yet, nice.');
-} else {
-  alert('Ha no kids I still have free time.');
-}
-
-let qFive = prompt('Was I in the military?').toLowerCase();
-if (qFive === 'yes'){
-  console.log('Its like you know me...');
-  alert('I was for a total of about 8 years.');
-} else {
-  alert('Sorry but I joined the Army at 19.');
-}
-
-/* 
-Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
-Indicates through an alert if the guess is "too high" or "too low".
-It should give the user exactly four opportunities to get the correct answer.
-After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
+  let tries = 4; // number of tries
 
 
- */
+  for (let i = 0; i < 4; i++) {
 
+    let qSix = prompt('Lets play a game, pick a number between 1-10.');
 
- let num = 5; // guessed number .
-
-let tries = 4; // number of tries
-
-
-for (let i = 0; i < 4; i++) {
-
-  let qSix = prompt('Lets play a game, pick a number between 1-10.');
-
-  if (qSix == num) {
-    alert (`You win! Correct number was ${num}`);
-  } else if (qSix > num) {
-    alert('Your guess is high!');
-  } else if (qSix < num) {
-    alert('Your guess is low!');
+    if (qSix == num) {
+      alert(`You win! Correct number was ${num}`);
+      break;
+    } else if (qSix > num) {
+      alert('Your guess is high!');
+    } else if (qSix < num) {
+      alert('Your guess is low!');
+    }
   }
-
   tries--;
   console.log(tries);
 }
 
-/* As a user, I would like to guess the answer to a question that could have many possibilities so that I can have fun with with a guessing game.
+numberGame();
 
-Add a 7th question that has multiple possible correct answers that are stored in an array.
-Give the user 6 attempts to guess the correct answer.
-The guesses will end once the user guesses a correct answer or they run out of attempts.
-Display all the possible correct answers to the user.
-Consider using a loop of some sort for this question.
-As a user, I would like to know my final score so that I can know how well I did.
+function hobbies() {
 
-Keep track of the total number of correct answers. At the end tell them how many they got correct out of the 7 questions asked. */
+  let ans = ['reading', 'sports', 'cooking', 'gaming', 'gardening', 'thrifting'];
 
-let ans = ['reading', 'sports', 'cooking', 'gaming', 'gardening', 'binge watching shows', 'thrifting', 'outdoors', 'anime'];
+  let guess = 6; // number of tries
+  let flag = false;
 
-let guess = 6; // number of tries
-let flag = false;
+  for (let i = 0; i < 6; i++) {
 
-for (let i = 0; i < 6; i++) {
+    let qSeven = prompt('What are some of the best Hobbies?');
 
-  let qSeven = prompt('What are some of the best Hobbies?');
+    for (let i = 0; i < ans.length; i++) {
 
-  for (let i = 0; i < ans.length; i++) {
+      if (qSeven === ans[i]) {
+        alert('You win!');
+        flag = true;
+        break;
+      }
+    }
 
-    if (qSeven === ans[i]) {
-      alert('You win!');
-      flag = true;
+    guess--;
+    console.log(guess);
+    if (flag === true) {
       break;
+    } else {
+      alert('Nope try again ' + guess + ' tries remaning.');
     }
   }
-
-  guess--;
-  console.log(guess);
-  if (flag === true) {
-    break;
-  } else {
-    alert('Nope try again ' + guess + ' tries remaning.');
-  }
 }
+
+hobbies();
+/* 
+let correctAnswers = qOne + qTwo  + qThree + qFour + qFive + qSix + qSeven;
+console.log(correctAnswers + "/7");
+alert("Thank you for taking this quiz " + name + " you got " + correctAnswers + " out of 7 right!"); */
